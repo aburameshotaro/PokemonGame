d4dc716a6fbb87c9900ab5aafb3656b0ea17a5f6
Sebastian Tomczok <jashinshotaro@gmail.com>
2023-08-17 21:06:23 +0200

    Deleting not needed auto-generated files

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PokemonGame.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
