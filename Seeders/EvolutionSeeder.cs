9d2e632c75a0db02a143f9b3c0e9eefce9d916ae
Sebastian Tomczok <jashinshotaro@gmail.com>
2023-11-16 15:52:51 +0100

    Evolution seeder created

---
+++ Seeders/EvolutionSeeder.cs
@@ -0,0 +1,16 @@
+﻿using PokemonGame.Entities;
+
+namespace PokemonGame.Seeders
+{
+    public class EvolutionSeeder : Seeder
+    {
+        public EvolutionSeeder(PokemonDbContext pokemonDb) : base(pokemonDb)
+        {
+        }
+
+        public override void Seed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
